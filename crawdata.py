3af0de87cc3f78faacd9cea0ec82054141156ed7
Nguyen Thi Nga <100331906+nguyenthingahd@users.noreply.github.com>
2023-12-08 17:16:58 +0700

    crawdata

---
+++ crawdata.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+listlink = []
+def crawl(baseUrl, url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    titles = soup.findAll('div', class_='title')
+    links = [link.find('a').attrs["href"] for link in titles]
+    # import pdb; pdb.set_trace()
+    for link in links:
+        if link not in listlink:
+            listlink.append(link)
+            try:
+                print(link)
+                news = requests.get(link)
+                soup = BeautifulSoup(news.content, "html.parser")
+                body = soup.find("div", id="nContent")
+                text = ""
+                content = body.findChildren("p", recursive=False)
+                text_file = open("output.txt", "a")
+                for i in range(0, len(content)):
+                    str = content[i].text
+                    sentences = str.split(".")
+                    for text in sentences:
+                        if len(text) > 1 and len(text) < 190:
+                            text = text.replace("- ", "")
+                            text = text.replace("\"", "")
+                            text = text.strip()
+                            text = text + ".\n"
+                            text_file.write(text)
+                text_file.close()
+            except:
+                pass
+if __name__ == "__main__":
+    f = open("url.txt")
+    str = f.read()
+    lst = str.split("\n")
+    baseUrl = "https://www.newsinlevels.com"
+    for url in lst:
+        crawl(baseUrl, url)
+        for i in range(2, 10):
+            urll = f"{baseUrl}/page/{i}/"  # Use f-string to format the URL
+            crawl(baseUrl, urll)
